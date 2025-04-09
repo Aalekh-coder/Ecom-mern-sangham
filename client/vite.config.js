a4bcef795eed39d51315f18af30e6559f4a75b52
Aalekh <dheetaalekh@gmail.com>
2025-04-09 09:58:18 +0530

    fsd

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "https://ecom-mern-sangham-backend.onrender.com",
+      "/api/": "https://ecom-mern-sangham-backend.onrender.com",
     },
   },
 });
