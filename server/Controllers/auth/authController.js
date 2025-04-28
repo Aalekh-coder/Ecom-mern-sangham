93c6f359df4826eb0515eb375ded2bd448391edd
Aalekh <dheetaalekh@gmail.com>
2025-04-28 20:33:36 +0530

    cookies

--- server/Controllers/auth/authController.js
+++ server/Controllers/auth/authController.js
@@ -62,10 +62,10 @@ const loginUser = async (req, res) => {
 
       },
       "getljni4736ver4g#2e3d",
-      { expiresIn: "10h" }
+      { expiresIn: "1d" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
       success: true,
       message: "Logged in successfully",
       user: {
